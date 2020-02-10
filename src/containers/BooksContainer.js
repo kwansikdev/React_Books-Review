72e2903331d56d004a1f063e7fc4ea2301d4acff
kwansikdev <kwansk0424@gmail.com>
2020-02-10 15:45:04 +0900

    feat: Revise connect with Hooks

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -1,20 +1,53 @@
-import { connect } from "react-redux";
+import React from "react";
+import { connect, useSelector, useDispatch } from "react-redux";
 import Books from "../components/Books";
-import { deleteBook, getBooks } from "../redux/modules/books";
+import { deleteBook, getBooks as getBooksAction } from "../redux/modules/books";
+import { useCallback } from "react";
 
-export default connect(
-  state => ({
-    token: state.auth.token,
-    books: state.books.books,
-    loading: state.books.loading,
-    error: state.books.error
-  }),
-  dispatch => ({
-    getBooks: async token => {
-      dispatch(getBooks(token));
+// connect
+// export default connect(
+//   state => ({
+//     token: state.auth.token,
+//     books: state.books.books,
+//     loading: state.books.loading,
+//     error: state.books.error
+//   }),
+//   dispatch => ({
+//     getBooks: async token => {
+//       dispatch(getBooks(token));
+//     },
+//     deleteBook: async (token, id) => {
+//       dispatch(deleteBook(token, id));
+//     }
+//   })
+// )(Books);
+
+// connect with Hooks
+
+const BooksContainer = props => {
+  // state
+  const { books, loading, error } = useSelector(state => state.books);
+
+  // dispatch
+  const dispatch = useDispatch();
+  const getBooks = useCallback(
+    async token => {
+      dispatch(getBooksAction());
     },
-    deleteBook: async (token, id) => {
-      dispatch(deleteBook(token, id));
-    }
-  })
-)(Books);
+    [dispatch]
+  );
+
+  return (
+    <>
+      <Books
+        {...props}
+        books={books}
+        loading={loading}
+        error={error}
+        getBooks={getBooks}
+      />
+    </>
+  );
+};
+
+export default BooksContainer;
